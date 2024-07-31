3d1d280ed8b8fa9cccc6b501dda7f5616898e244
jfatras <jjcfatras@gmail.com>
2024-07-31 10:54:48 -0400

    test: cleanup lint-staged

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,9 +1,5 @@
 module.exports = {
-  "*.{js,jsx,cjs,mjs,ts,tsx}": [
-    "tsc-files --noEmit",
-    "eslint --fix",
-    "prettier --write",
-  ],
+  "*.{ts,tsx}": ["tsc-files --noEmit", "eslint --fix", "prettier --write"],
   "*.{js,jsx,cjs,mjs}": ["eslint --fix", "prettier --write"],
   "*.{css,scss,html,json,graphql}": ["prettier --write"],
 };
